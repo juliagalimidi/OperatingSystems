f9e69da0395a4cc683905adf4a157ca54077aacb
jamison rayfield <jamisonrayfield@gmail.com>
2018-03-21 18:13:36 -0400

    Add files via upload

--- LKMChar.c
+++ LKMChar.c
@@ -1,13 +1,6 @@
 /* 
 * @file   LKMChar.c
- * @author Derek Molloy
- * @date   7 April 2015
- * @version 0.1
- * @brief   An introductory character driver to support the second article of my series on
- * Linux loadable kernel module (LKM) development. This module maps to /dev/LKMChar and
- * comes with a helper C program that can be run in Linux user space to communicate with
- * this the LKM.
- * @see http://www.derekmolloy.ie/ for a full description and follow-up descriptions.
+ * 
  */
  
 #include <linux/init.h>           // Macros used to mark up functions e.g. __init __exit
