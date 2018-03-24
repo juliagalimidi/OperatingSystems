f440e72fee5a4ec1d9db2e1194f820c1bb213d06
Julia Galimidi <juliagalimidi@yahoo.com>
2018-03-23 23:58:14 -0400

    Adding ability to scan in multiple strings
    we need a way to prompt user to read
    read doesn't print print correctly

--- LKMChartest.c
+++ LKMChartest.c
@@ -8,26 +8,45 @@
 #include<string.h>
 #include<unistd.h>
  
-#define BUFFER_LENGTH 256               ///< The buffer length (crude but fine)
+#define BUFFER_LENGTH 1024              //Store bytes written to it up to a constant buffer size (at least 1KB)
 static char receive[BUFFER_LENGTH];     ///< The receive buffer from the LKM
- 
+int currentBuffSize = 0;
 int main(){
-   int ret, fd;
+   int ret, fd, i, j = 0;
    char stringToSend[BUFFER_LENGTH];
    printf("Starting device test code example...\n");
    fd = open("/dev/LKMChar", O_RDWR);             // Open the device with read/write access
-   if (fd < 0){
+   if (fd < 0)
+   {
       perror("Failed to open the device...");
       return errno;
    }
-   printf("Type in a short string to send to the kernel module:\n");
-   scanf("%[^\n]%*c", stringToSend);                // Read in a string (with spaces)
-   printf("Writing message to the device [%s].\n", stringToSend);
-   ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
-   if (ret < 0){
-      perror("Failed to write the message to the device.");
-      return errno;
-   }
+	while(currentBuffSize + strlen(stringToSend) < BUFFER_LENGTH)
+   {
+		printf("Type in a short string to send to the kernel module:\n");
+		scanf("%[^\n]%*c", stringToSend);                // Read in a string (with spaces)
+		printf("Writing message to the device [%s].\n", stringToSend);
+		ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
+		currentBuffSize += strlen(stringToSend);
+		if (ret < 0)
+		{
+			perror("Failed to write the message to the device.");
+			return errno;
+		}
+   } 
+	if(currentBuffSize < BUFFER_LENGTH)
+	{
+		int spaceRemaining = (currentBuffSize + strlen(stringToSend)) - BUFFER_LENGTH;
+		char modStringToSend[spaceRemaining];
+		
+		for(i = currentBuffSize; i < BUFFER_LENGTH; i++)
+		{
+			modStringToSend[j] = stringToSend[j];
+		}
+		ret = write(fd, modStringToSend, strlen(stringToSend));
+		currentBuffSize += strlen(modStringToSend);
+	}
+   
  
    printf("Press ENTER to read back from the device...\n");
    getchar();
